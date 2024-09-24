1dcaac065afd1ae8953b5b290743bc9c8e91a25a
SethupathyD <sethupathy.d@syncfusion.com>
2024-09-24 15:05:35 +0530

    Sample regarding android vertical Scroll Bar Visibility issue.

---
+++ SimpleSample/SimpleSample/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
